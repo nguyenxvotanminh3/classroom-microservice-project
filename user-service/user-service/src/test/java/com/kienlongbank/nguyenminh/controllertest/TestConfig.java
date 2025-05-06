537289d64d604f84b66e2a3a26f5db022629f8c3
nguyenxvotanminh3 <lop123thcs@gmail.com>
2025-05-06 17:17:56 +0700

    delete old user test

--- user-service/user-service/src/test/java/com/kienlongbank/nguyenminh/controllertest/TestConfig.java
+++
@@ -1,8 +0,0 @@
-package com.kienlongbank.nguyenminh.controllertest;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration
-public class TestConfig {
-    // Thêm các bean mock nếu cần
-} 
